3c8a4d52b0d66645eb5f2e738c9675fcdcbedce2
guanweisong <307761682@qq.com>
2023-05-26 08:40:52 +0800

    chore: update cors middleware

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default async function middleware(request: NextRequest) {
   const origin = requestHeaders.get('origin');
   response.headers.set('Access-Control-Allow-Origin', origin!);
   response.headers.set('Access-Control-Allow-Credentials', 'true');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   response.headers.set(
     'Access-Control-Allow-Headers',
     'content-type, x-auth-token, x-requested-with',
