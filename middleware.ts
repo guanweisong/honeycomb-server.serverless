4c69a16c162d4e1b59532b87ec00b130af55b344
guanweisong <307761682@qq.com>
2023-05-26 09:08:48 +0800

    chore: update cors middleware

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,19 @@ import { NextResponse } from 'next/server';
 
 export default async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
   const origin = requestHeaders.get('origin');
-  response.headers.set('Access-Control-Allow-Origin', origin!);
-  response.headers.set('Access-Control-Allow-Credentials', 'true');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  response.headers.set(
+
+  requestHeaders.set('Access-Control-Allow-Origin', origin!);
+  requestHeaders.set('Access-Control-Allow-Credentials', 'true');
+  requestHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  requestHeaders.set(
     'Access-Control-Allow-Headers',
     'content-type, x-auth-token, x-requested-with',
   );
 
-  return response;
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
