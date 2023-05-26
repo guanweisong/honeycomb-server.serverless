8c783dab857defd024945cfd400e1b0e5db16268
guanweisong <307761682@qq.com>
2023-05-26 08:36:59 +0800

    chore: update cors middleware

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,20 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export default async function middleware(request: NextRequest) {
-  // const response = NextResponse.next();
-  //
-  // const origin = request.headers.get('origin');
-  // response.headers.set('Access-Control-Allow-Origin', origin!);
-  // response.headers.set('Access-Control-Allow-Credentials', 'true');
-  // response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  // response.headers.set(
-  //   'Access-Control-Allow-Headers',
-  //   'content-type, x-auth-token, x-requested-with',
-  // );
-  //
-  // return response;
+  const requestHeaders = new Headers(request.headers);
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  const origin = requestHeaders.get('origin');
+  response.headers.set('Access-Control-Allow-Origin', origin!);
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'content-type, x-auth-token, x-requested-with',
+  );
+
+  return response;
 }
