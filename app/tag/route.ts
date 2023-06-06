7a9cc935a947cdd3922f194edd6daf610cae18b0
guanweisong <307761682@qq.com>
2023-06-06 08:42:33 +0800

    chore: add edge runtime

--- app/tag/route.ts
+++ app/tag/route.ts
@@ -9,6 +9,8 @@ import { UserLevel } from '.prisma/client';
 import { validateAuth } from '@/libs/validateAuth';
 import { getQueryParams } from '@/libs/getQueryParams';
 
+export const runtime = 'edge';
+
 export async function GET(request: NextRequest) {
   const validate = TagListQuerySchema.safeParse(getQueryParams(request));
   if (validate.success) {
