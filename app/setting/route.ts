11a4ed9f4a0f62d894e50bfede6163acf8895f66
guanweisong <307761682@qq.com>
2023-06-07 10:08:22 +0800

    chore: make setting api dynamic

--- app/setting/route.ts
+++ app/setting/route.ts
@@ -1,6 +1,8 @@
 import prisma from '@/libs/prisma';
 import ResponseHandler from '@/libs/responseHandler';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const result = await prisma.setting.findFirst();
   return ResponseHandler.Query(result);
