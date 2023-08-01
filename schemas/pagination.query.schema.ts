339407abd1827227908fc2f825715bb4e5e30b8d
guanweisong <307761682@qq.com>
2023-08-01 08:51:28 +0800

    fix: Converted the sort enum value

--- schemas/pagination.query.schema.ts
+++ schemas/pagination.query.schema.ts
@@ -1,8 +1,22 @@
 import { z } from 'zod';
 
+export enum AntdSortOrder {
+  descend = 'descend',
+  ascend = 'ascend',
+}
+
+const mapAntdSortOrder = {
+  [AntdSortOrder.ascend]: 'asc',
+  [AntdSortOrder.descend]: 'desc',
+};
+
+const antdSortOrderEnum = z.nativeEnum(AntdSortOrder);
+
 export const PaginationQuerySchema = z.object({
   page: z.number().min(1, '最小页码是1').default(1),
   limit: z.number().min(1, '最小查询1条记录').default(10),
   sortField: z.string().default('createdAt'),
-  sortOrder: z.string().default('desc'),
+  sortOrder: antdSortOrderEnum
+    .default(AntdSortOrder.descend)
+    .transform((val) => mapAntdSortOrder[val]),
 });
