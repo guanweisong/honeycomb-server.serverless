c38717e86f8dc407a4dd2cabc6de776a52825487
guanweisong <307761682@qq.com>
2023-08-29 09:01:20 +0800

    feat: simplify the data returned by the interface

--- app/post/[id]/views/route.ts
+++ app/post/[id]/views/route.ts
@@ -4,14 +4,16 @@ import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
-  const result = await prisma.post
-    .findUnique({ where: { id } })
-    .then((result) => ({ count: result?.views }));
+  const result = await prisma.post.findUnique({ where: { id }, select: { views: true } });
   return ResponseHandler.Query(result);
 }
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
-  const result = await prisma.post.update({ where: { id }, data: { views: { increment: 1 } } });
+  const result = await prisma.post.update({
+    where: { id },
+    data: { views: { increment: 1 } },
+    select: { views: true },
+  });
   return ResponseHandler.Update(result);
 }
