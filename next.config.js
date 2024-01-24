a1eb89799ea3632652496a92538794923141c47a
guanweisong <307761682@qq.com>
2024-01-24 08:27:34 +0800

    Chore: remove useless config

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
     LINK_OBJECT_ID: '5349b4ddd2781d08c09890f3',
     FRONT_DOMAIN: 'www.guanweisong.com',
   },
-  experimental: {
-    serverComponentsExternalPackages: [
-      '@react-email/components',
-      '@react-email/render',
-      '@react-email/html',
-    ],
-  },
-  transpilePackages: ['@react-email/components', '@react-email/render', '@react-email/html'],
 };
 
 module.exports = nextConfig;
